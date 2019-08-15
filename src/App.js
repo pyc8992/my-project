364dbf820fde8dd159adcb493ab60ddc535d6677
docki <pyc8992dev@gmail.com>
2019-08-15 13:19:50 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import Potato from './Potato';
+
+function Food({ fav }) {
+
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
     <div> 
       Hello!!
-      <Potato />
+      {/* 동적으로 Property를 만들어서 하위 컴포넌트로 보낼 수 있음 */}
+      {/* JSX = HTML + Javascript */}
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="sangiopsal" />
+      <Food fav="chukumi" />
     </div>
   );
 }
