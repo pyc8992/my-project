77c65b3f841617efcfc8d99fd9ab5bcab95edc43
docki <pyc8992dev@gmail.com>
2019-08-17 22:31:07 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -1,45 +1,58 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
     id:0,
     name: "Kimchi",
     image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 4.9
   },
   {
     id:1,
     name: "Samgyeopsal",
     image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+    rating: 4.3,
   },
   {
     id:2,
     name: "Bibimbap",
     image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+    rating: 4.1
   },
   {
     id:3,
     name: "Doncasu",
     image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 4.2
   },
   {
     id:4,
     name: "Kimbap",
     image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 4.1,
   }
 ];
 
-function Food({ name, picture }) {
+function Food({ name, picture, rating }) {
 
   return <div>
     <h1>I like {name}</h1>
+    <h4>{rating} / 5.0 </h4>
     <img src={picture} alt={name} />
   </div>
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 function App() {
   return (
@@ -47,7 +60,7 @@ function App() {
       Hello!!
       {/* 동적으로 Property를 만들어서 하위 컴포넌트로 보낼 수 있음 */}
       {/* JSX = HTML + Javascript */}
-      {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} />)}
+      {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />)}
     </div>
   );
 }
