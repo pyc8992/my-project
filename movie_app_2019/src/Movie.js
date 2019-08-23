c80e248fb37113500a191db876ef41b2bb16a78f
docki <pyc8992dev@gmail.com>
2019-08-23 22:50:14 +0900

    updated

--- movie_app_2019/src/Movie.js
+++ movie_app_2019/src/Movie.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 function Movie({year, title, summary, poster, genres}) {
     return <div className="movie">
@@ -7,7 +8,7 @@ function Movie({year, title, summary, poster, genres}) {
         <div className="movie__data">    
             <h3 className="movie__title">{title}</h3>
             <h5 className="movie__year">{year}</h5>
-            <ul className="genres">{genres.map((genre, index) => <li key={index} className="genres__genre">{genre}</li> )}</ul>
+            <ul className="movie__genres">{genres.map((genre, index) => <li key={index} className="genres__genre">{genre}</li> )}</ul>
             <p className="movie__summary">{summary}</p>
         </div>
     </div>
