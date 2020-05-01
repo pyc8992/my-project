ae307c47d82226f8fd407df42d43d4043c24aca9
Jayden <pyc8992dev@gmail.com>
2020-05-01 16:07:17 +0900

    ts-node init

---
+++ pr-ts-node/back/index.js
@@ -0,0 +1,54 @@
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var morgan = require("morgan");
+var cors = require("cors");
+var cookieParser = require("cookie-parser");
+var expressSession = require("express-session");
+var dotenv = require("dotenv");
+var passport = require("passport");
+var hpp = require("hpp");
+var helmet = require("helmet");
+dotenv.config();
+var app = express();
+var prod = process.env.NODE_ENV === 'production';
+app.set('port', prod ? process.env.PORT : 4000);
+if (prod) {
+    app.use(hpp());
+    app.use(helmet());
+    app.use(morgan('combined'));
+    app.use(cors({
+        origin: /docki\.com$/,
+        credentials: true
+    }));
+}
+else {
+    app.use(morgan('dev'));
+    app.use(cors({
+        origin: true,
+        credentials: true
+    }));
+}
+app.use('/', express.static('uploads'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+        domain: prod ? '.docki.com' : undefined
+    },
+    name: 'test'
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.get('/', function (req, res) {
+    res.send('success');
+});
+app.listen(app.get('port'), function () {
+    console.log("server is running on " + app.get('port'));
+});
